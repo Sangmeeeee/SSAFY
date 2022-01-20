e814e159fd63413a18c5380de891caeb854eae1c
nullyng <young315@knu.ac.kr>
2022-01-20 21:08:47 +0900

    day1 commit

---
+++ 1주차/김주영/Book.java
@@ -0,0 +1,76 @@
+package com.ssafy.ws04.step3;
+
+public class Book {
+	private String isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private int price;
+	private String desc;
+	
+	public Book() {
+		
+	}
+	
+	public Book(String isbn, String title, String author, String publisher, int price, String desc) {
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+		this.desc = desc;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	public String toString() {
+		return isbn + '\t' + "| " + title + '\t' + "| " + author + '\t' + "| " + publisher + '\t' 
+				+ "| " + price + '\t' + "| " + desc + '\t'; 
+	}
+}
