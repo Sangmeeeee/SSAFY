cf3cdfc6261a6c2cbfca5ff3173941d3f66b953c
inseok9876@gmail.com <inseok9876@gmail.com>
2022-01-24 20:16:06 +0900

    inseok/연습

---
+++ week1/inseok/Problem1/Problem1.java
@@ -0,0 +1,12 @@
+package w0124.test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem1 {
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		
+	}
+}
