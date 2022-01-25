6f6af016b93179b027c5da6637437993c6c60c4b
Inhu Jo <inhudev@gmail.com>
2022-01-26 01:35:09 +0900

    inu/17140 이차원 배열과 연산

--- week1/inu/problem2/Problem2.cpp
+++ week1/inu/problem2/Problem2.cpp
@@ -5,103 +5,81 @@
 using namespace std;
 
 int A[100][100];
+int R = 3, C = 3;
+
+void check(bool line, int i, int range, int& max_size) {
+	int cnt[101] = { 0, };
+	vector<pair<int, int>> v;
+
+	if (line == true) {
+		for (int j = 0; j < range; j++)
+			if (A[i][j] != 0)
+				cnt[A[i][j]]++;
+	}
+	else {
+		for (int j = 0; j < range; j++)
+			if (A[j][i] != 0)
+				cnt[A[j][i]]++;
+	}
+
+	for (int k = 1; k <= 100; k++)
+		if (cnt[k] != 0)
+			v.push_back({ cnt[k], k });
+	sort(v.begin(), v.end());
+
+	int idx = 0;
+	int q_size = v.size() * 2 >= 100 ? 100 : v.size() * 2;
+	if (line == true) {
+		for (int j = 0; j < q_size; j += 2) {
+			A[i][j] = v[idx].second;
+			A[i][j + 1] = v[idx++].first;
+		}
+
+		for (int j = q_size; j < range; j++)
+			A[i][j] = 0;
+	}
+	else {
+		for (int j = 0; j < q_size; j += 2) {
+			A[j][i] = v[idx].second;
+			A[j + 1][i] = v[idx++].first;
+		}
+
+		for (int j = q_size; j < range; j++)
+			A[j][i] = 0;
+	}
+
+	max_size = q_size > max_size ? q_size : max_size;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int R = 3, C = 3;
 	int r, c, k;
 	int time = 0;
-	int max_num = 0;
 
 	cin >> r >> c >> k;
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 			cin >> A[i][j];
 
-	// 100번만 반복
 	for (int t = 0; t <= 100; ++t) {
-		// 맞으면 시간 출력하고 종료
 		if (A[r - 1][c - 1] == k) {
 			cout << t;
 			return 0;
 		}
 
-		int q_size;
 		int max_size = 0;
-		vector<pair<int, int>> v;
 
-		// 행 개수 >= 열 개수
 		if (R >= C) {
-			for (int i = 0; i < R; i++) {
-				int cnt[101] = { 0, };
-
-				// 등장 횟수 카운트
-				// 현재 배열에서 가장 큰 수를 찾아 max_num에 저장
-				for (int j = 0; j < C; j++)
-					if (A[i][j] != 0) {
-						cnt[A[i][j]]++;
-					}
-
-				// max_num까지만 cnt배열 조회
-				// {등장 횟수, 숫자}로 벡터에 넣고 정렬
-				for (int k = 1; k <= 100; k++)
-					if (cnt[k] != 0)
-						v.push_back({ cnt[k], k });
-				sort(v.begin(), v.end());
-
-				// 벡터 크기 * 2가 100을 넘지 않도록 조정
-				q_size = v.size() * 2 >= 100 ? 100 : v.size() * 2;
-
-				int idx = 0;
-				for (int j = 0; j < q_size; j += 2) {
-					A[i][j] = v[idx].second;
-					A[i][j + 1] = v[idx++].first;
-				}
-
-				// q_size를 넘어가는 부분은 0으로 바꿔줌
-				for (int j = q_size; j < C; j++)
-					A[i][j] = 0;
-
-				// 현재 정렬에서 가장 큰 열의 개수를 구해놓고 정렬 끝나고 바꿔줌
-				max_size = q_size > max_size ? q_size : max_size;
-
-				v.clear();
-			}
+			for (int i = 0; i < R; i++)
+				check(true, i, C, max_size);
 			C = max_size;
 		}
-		// 행 개수 < 열 개수
 		else {
-			for (int i = 0; i < C; i++) {
-				int cnt[101] = { 0, };
-
-				for (int j = 0; j < R; j++)
-					if (A[j][i] != 0) {
-						cnt[A[j][i]]++;
-					}
-
-				for (int k = 1; k <= 100; k++)
-					if (cnt[k] != 0)
-						v.push_back({ cnt[k], k });
-				sort(v.begin(), v.end());
-
-				q_size = v.size() * 2 >= 100 ? 100 : v.size() * 2;
-
-				int idx = 0;
-				for (int j = 0; j < q_size; j += 2) {
-					A[j][i] = v[idx].second;
-					A[j + 1][i] = v[idx++].first;
-				}
-
-				for (int j = q_size; j < R; j++)
-					A[j][i] = 0;
-
-				max_size = q_size > max_size ? q_size : max_size;
-
-				v.clear();
-			}
+			for (int i = 0; i < C; i++)
+				check(false, i, R, max_size);
 			R = max_size;
 		}
 	}
