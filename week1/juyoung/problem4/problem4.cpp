bbae8e51bc43e46f7776b673b07a8f60465c34f0
nullyng <young315@knu.ac.kr>
2022-01-28 20:32:42 +0900

    juyoung/프로그래머스 괄호 회전하기

--- week1/juyoung/problem4/problem4.cpp
+++ week1/juyoung/problem4/problem4.cpp
@@ -1,39 +1,21 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int N, K;
-vector<int> sensor;	// 센서 위치 저장
-vector<int> diff;	// 센서 간의 거리 저장
-int answer = 0;
+int solution(string s) {
+	int answer = 0;
 
-int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
-		int location;
-		cin >> location;
-		sensor.push_back(location);
+	for (int i = 0; i < s.size(); i++) {
+		int n = s[0];
+		s.erase(0, 1);
+		s.push_back(n);
+		cout << s << "\n";
 	}
 
-	// 센서의 위치를 오름차순으로 정렬하고 중복 제거
-	sort(sensor.begin(), sensor.end());
-	sensor.erase(unique(sensor.begin(), sensor.end()), sensor.end());
+	return answer;
+}
 
-	// 인접한 센서 간의 거리 계산 후 저장
-	for (int i = 0; i < (int)sensor.size() - 1; i++)
-		diff.push_back(sensor[i + 1] - sensor[i]);
-
-	// 센서 간의 거리 오름차순으로 정렬
-	// 정렬한 벡터의 제일 뒤에서 (집중국의 수-1)만큼은 빼고 전부 더한 것이 답
-	sort(diff.begin(), diff.end());
-	for (int i = 0; i < (int)diff.size() - K + 1; i++)
-		answer += diff[i];
-
-	cout << answer;
-
+int main() {
+	solution("[](){}");
 	return 0;
 }
