22b9d8d71560a62e18be6ff59a52e8b858954b36
Inhu Jo <inhudev@gmail.com>
2022-05-26 18:20:38 +0900

    inu/주차요금

---
+++ week18/inu/problem4/Problem4.cpp
@@ -0,0 +1,88 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<string> split(string s, char delimeter) {
+    vector<string> splitted;
+    stringstream input(s);
+    string temp;
+    while (getline(input, temp, delimeter)) {
+        splitted.push_back(temp);
+    }
+    return splitted;
+}
+
+int getParkingTime(string start, string end) {
+    int shour = stoi(start.substr(0, 2));
+    int smin = stoi(start.substr(3, 2));
+    int ehour = stoi(end.substr(0, 2));
+    int emin = stoi(end.substr(3, 2));
+    return ehour * 60 + emin - shour * 60 - smin;
+}
+
+int getCharge(vector<int> fees, int time) {
+    int charge = fees[1];
+    if (time > fees[0]){
+        int remain = (time - fees[0]) / fees[2];
+        if ((time - fees[0]) % fees[2] != 0) remain++;
+        charge += remain * fees[3];
+    }
+    return charge;
+}
+
+// fees: 기본시간, 기본요금, 단위시간, 단위요금
+vector<int> solution(vector<int> fees, vector<string> records) {
+    vector<int> answer;
+    unordered_map<string, string> inout;
+    unordered_map<string, int> time;
+    vector<string> car_number;
+
+    for (string s : records) {
+        vector<string> record = split(s, ' ');
+
+        if (!record[2].compare("IN")) {
+            inout[record[1]] = record[0];
+            if (time.count(record[1]) == 0) {
+                time[record[1]] = 0;
+                car_number.push_back(record[1]);
+            }
+        }
+        else {
+            int parkingTime = getParkingTime(inout[record[1]], record[0]);
+            time[record[1]] += parkingTime;
+            inout.erase(record[1]);
+        }
+    }
+
+    for (auto i : inout) {
+        int parkingTime = getParkingTime(i.second, "23:59");
+        time[i.first] += parkingTime;
+    }
+
+    sort(car_number.begin(), car_number.end());
+
+    for (string s : car_number) {
+        int charge = getCharge(fees, time[s]);
+        answer.push_back(charge);
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> fees1 = { 180, 5000, 10, 600 };
+    vector<int> fees2 = { 120, 0, 60, 591 };
+    vector<int> fees3 = { 1, 461, 1, 10 };
+
+    vector<string> records1 = { "05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT" };
+    vector<string> records2 = { "16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN" };
+    vector<string> records3 = { "00:00 1234 IN" };
+
+    vector<int> answer1 = solution(fees1, records1);
+    vector<int> answer2 = solution(fees2, records2);
+    vector<int> answer3 = solution(fees3, records3);
+}
---
+++ week18/inu/problem4/readme.md
@@ -0,0 +1,30 @@
+# Info
+
+  
+
+문제 : [주차요금](https://programmers.co.kr/learn/courses/30/lessons/92341)
+
+알고리즘 종류 : 
+
+참조 url : 
+  
+
+# 알고리즘 설명
+`record` [시각, 차량번호, 내역]을 저장하는 vector  
+`inout` <차량번호, 입차시간>을 저장하는 map  
+`time` <차량번호, 주차시간>을 저장하는 map  
+`car_number` 차량번호를 저장하는 set
+`answer` 각 차량의 주차요금을 저장하는 vector
+
+
+1. `record`의 내역이 "IN"이면 `inout`에 <차량번호, 입차시간>을 insert한다.
+    - 이 때, `car_number`에 해당 차량번호가 있는지 확인한다.
+    - 차량번호가 없으면 `car_number`에 insert하고 `time`에도 <차량번호, 0>을 insert한다.
+2. `record`의 내역이 "OUT"이면 `inout`에서 해당 차량번호의 입차시간을 꺼내서 주차시간을 계산한다.
+    - `time[차량번호]`에 계산한 주차시간을 더해준다.
+3. 모든 `record`를 확인했으면 23:59를 출차시간으로 해서 `inout`에 남은 차량들의 주차시간을 구한다.
+4. `car_number`에 저장된 차량번호 순서대로 주차요금을 구해서 `answer`에 넣는다.
+   - `car_number`는 set이기 때문에 오름차순을 디폴트로 정렬이 된다.
+
+
+# 새로 배운 내용
